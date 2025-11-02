118b1bc0440e57db189d4b2612771b51a7b48acc
atharvash1512 <atharvash1326@gmail.com>
2025-11-03 00:32:10 +0530

    Create ExampleApp.java

---
+++ ExampleApp.java
@@ -0,0 +1,7 @@
+import java.util.*;
+
+public class ExampleApp {
+    public static void main(String[] args) {
+        System.out.println("This is an example of a Java code application");
+    }
+}
