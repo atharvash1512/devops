e68e0ebb31627b4036be08f8eba894f4b14e0489
atharvash1512 <atharvash1326@gmail.com>
2025-11-03 11:08:48 +0530

    Implement Selenium WebDriver to open Google
    Added Selenium WebDriver functionality to open Google and print the page title.

--- Sel.java
+++ Sel.java
@@ -3,3 +3,24 @@ public class Sel {
         System.out.println("Hello from Jenkins Selenium Project!");
     }
 }
+
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Sel {
+    public static void main(String[] args) {
+        // Start a browser (this is Selenium)
+        WebDriver driver = new ChromeDriver();
+
+        // Open a website
+        driver.get("https://www.google.com");
+
+        // Print the page title
+        System.out.println("Page title: " + driver.getTitle());
+
+        // Close the browser
+        driver.quit();
+    }
+}
